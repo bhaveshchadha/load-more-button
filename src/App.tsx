9c3632cd6f849482a26d5baa29b8e92b275b7df6
bhaveshchadha <bhaveshchadha14@gmail.com>
2024-09-23 04:43:47 +0530

    ADD added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="grid grid-cols-4 gap-4">
+        <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
+      </div>
+    </>
+  );
 }
 
 export default App;
